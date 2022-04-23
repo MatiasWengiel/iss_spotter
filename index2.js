93c37607fc823a892f771ac9a0dae460e7ee77b0
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-23 10:47:52 -0500

    Fixed linting errors

--- index2.js
+++ index2.js
@@ -2,8 +2,8 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
 
 const printPassTimes = (passTimes) => {
   for (let eachTime of passTimes) {
-    const date = new Date(eachTime.risetime * 1000) //Time provided in passTimes is in seconds from 01/01/70 rather than ms
-    console.log(`Next pass at ${date}, for ${eachTime.duration} seconds`)
+    const date = new Date(eachTime.risetime * 1000); //Time provided in passTimes is in seconds from 01/01/70 rather than ms
+    console.log(`Next pass at ${date}, for ${eachTime.duration} seconds`);
   }
    
 };
@@ -14,7 +14,7 @@ nextISSTimesForMyLocation()
   })
   .catch((error) => {
     console.log("It didn't work! ", error.message);
-  })
+  });
 
 
 
--- iss_promised.js
+++ iss_promised.js
@@ -1,28 +1,28 @@
 const request = require('request-promise-native');
 
 const fetchMyIP = () => {
-  return request('https://api.ipify.org?format=json')
-}
+  return request('https://api.ipify.org?format=json');
+};
 
 const fetchCoordsByIP = (body) => {
-  const ip = JSON.parse(body).ip
-  return request(`https://api.freegeoip.app/json/${ip}?apikey=4d862900-c25f-11ec-8bac-c52bfff9dffd`)
-}
+  const ip = JSON.parse(body).ip;
+  return request(`https://api.freegeoip.app/json/${ip}?apikey=4d862900-c25f-11ec-8bac-c52bfff9dffd`);
+};
 
 const fetchISSFlyOverTimes = (body) => {
   const latitude = JSON.parse(body).latitude;
-  const longitude = JSON.parse(body).longitude
+  const longitude = JSON.parse(body).longitude;
   
-  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
-}
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+};
 
 const nextISSTimesForMyLocation = () => {
   return fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then((body) => {
-    const { response } = JSON.parse(body);
-    return response
-  });
-}
-module.exports = { nextISSTimesForMyLocation }
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((body) => {
+      const { response } = JSON.parse(body);
+      return response;
+    });
+};
+module.exports = { nextISSTimesForMyLocation };
