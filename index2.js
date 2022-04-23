99256f01ea328cdabfb9535a200d1ee164676668
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-23 10:41:44 -0500

    Refactored files to use promises rather than nested callbacks

---
+++ index2.js
@@ -0,0 +1,22 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = (passTimes) => {
+  for (let eachTime of passTimes) {
+    const date = new Date(eachTime.risetime * 1000) //Time provided in passTimes is in seconds from 01/01/70 rather than ms
+    console.log(`Next pass at ${date}, for ${eachTime.duration} seconds`)
+  }
+   
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work! ", error.message);
+  })
+
+
+
+
+
